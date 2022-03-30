8e3be56bde97774d28c5e699fd099a29b9c595ab
Alex-Horman-de-Medeiros-Correia <alexhorman1990@gmail.com>
2022-03-29 21:12:17 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('testa o componente notfound', () => {
+  it('deve ter um heading h2 com o texto Page requested not found e um emoji', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFound = screen
+      .getByRole('heading', { level: 2, name: /Page requested not found/i });
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('deve mostrar uma imagem na tela', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const pikachu = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(pikachu).toBeInTheDocument();
+
+    expect(pikachu).toHaveProperty('src', image);
+  });
+});
